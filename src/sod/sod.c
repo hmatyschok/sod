623eb9864dadf17c4fd5e78a0241e84a1f2599ae
gast <henning.matyschok@stud.fh-flensburg.de>
2016-07-11 14:16:14 +0200

    Transformation into forking server.
     This commit won't compile yet, because
     the intention is to show progress about
     recent developement activities.
    
     The complexity reduced from structure
     of child process.

--- src/sod/sod.c
+++ src/sod/sod.c
@@ -57,6 +57,7 @@ struct sod_softc {
     
     int     sc_sock_srv;     /* fd, socket, applicant */
     int     sc_sock_rmt;     /* fd, socket, applicant */
+    
     int     sc_eval;     /* tracks rv of pam(3) method calls */ 
 };
 #define    SOD_BACKOFF_DFLT     3
@@ -64,12 +65,6 @@ struct sod_softc {
 
 #define    SOD_PROMPT_DFLT        "login: "
 #define    SOD_PW_PROMPT_DFLT    "Password:"
-
-/*
- * Recursively defined callback function. 
- */
-typedef long     (*sod_state_fn_t)(struct sod_softc *);
-typedef sod_state_fn_t     (*sod_state_t)(struct sod_softc *);
 
 static pid_t     pid;
 static pthread_t     tid;
@@ -98,8 +93,6 @@ static void *    sod_sigaction(void *);
 static int     sod_conv(int, const struct pam_message **, 
     struct pam_response **, void *);
 
-static sod_state_fn_t     sod_authenticate(struct sod_softc *);
-static sod_state_fn_t     sod_establish(struct sod_softc *);
 static void     sod_doit(int, int);
 
 /*
@@ -227,8 +220,16 @@ static void
 sod_doit(int s, int r)
 {
     struct sod_softc sc;
-    sod_state_t fn;
     
+    login_cap_t *sc_lc;
+    
+    const char     *prompt;
+    const char     *pw_prompt;
+    
+    int retries, backoff;
+    int ask = 1, cnt = 0;
+    uint32_t resp;
+
     (void)memset(&sc, 0, sizeof(sc));
     
     sc.sc_sock_rmt = r;
@@ -236,15 +237,112 @@ sod_doit(int s, int r)
     
     sc.sc_pamc.appdata_ptr = &sc;
     sc.sc_pamc.conv = sod_conv;
-    
-    fn = (sod_state_t)sod_establish;
-    
-    while (fn != NULL)
-        fn = (sod_state_t)(*fn)(&sc);
+/*
+ * Create < hostname, user > tuple.
+ */
+    if (sod_msg_fn(sod_msg_recv, sc.sc_sock_rmt, &sc.sc_buf) < 0) 
+        goto out;
 
+    if (gethostname(sc.sc_host, SOD_NMAX) < 0) 
+        goto out;    
+ 
+    (void)strncpy(sc.sc_user, sc.sc_buf.sm_tok, SOD_NMAX);
+/*
+ * Verify, if username exists in passwd database. 
+ */
+    if ((sc.sc_pwd = getpwnam(sc.sc_user)) != NULL) {
+/*
+ * Verify, if user has UID 0, because login by UID 0 is not allowed. 
+ */
+        if (sc.sc_pwd->pw_uid == (uid_t)0) 
+            sc.sc_eval = PAM_PERM_DENIED;
+        else
+            sc.sc_eval = PAM_SUCCESS;
+    } else 
+        sc.sc_eval = PAM_USER_UNKNOWN;
+    
+    endpwent();          
+
+    if (sc.sc_eval != PAM_SUCCESS)
+        goto out;
+        
+    if (sc.sc_buf.sm_code == SOD_AUTH_REQ) {  
+/*
+ * Parts of in login.c defined codesections are reused here.
+ */    
+        lc = login_getclass(NULL);
+        prompt = login_getcapstr(lc, "login_prompt", 
+            sod_prompt_default, sod_prompt_default);
+        pw_prompt = login_getcapstr(lc, "passwd_prompt", 
+            sod_pw_prompt_default, sod_pw_prompt_default);
+        retries = login_getcapnum(lc, "login-retries", 
+            SOD_RETRIES_DFLT, SOD_RETRIES_DFLT);
+        backoff = login_getcapnum(lc, "login-backoff", 
+            SOD_BACKOFF_DFLT, SOD_BACKOFF_DFLT);
+        login_close(lc);
+        lc = NULL;
+       
+        while (ask != 0) {
+/*
+ * Open pam(8) session and authenticate.
+ */        
+            sc.sc_eval = pam_start(sod_name, sc.sc_user, 
+                &sc.sc_pamc, &sc.sc_pamh);
+
+            if (sc.sc_eval == PAM_SUCCESS) {
+                sc.sc_eval = pam_set_item(sc.sc_pamh, PAM_RUSER, 
+                    sc.sc_user);
+            }
+    
+            if (sc.sc_eval == PAM_SUCCESS) {
+                sc.sc_eval = pam_set_item(sc.sc_pamh, PAM_RHOST, 
+                    sc.sc_host);
+            }
+    
+            if (sc.sc_eval == PAM_SUCCESS) {
+                sc.sc_eval = pam_authenticate(sc.sc_pamh, 0);
+/*
+ * Authenticate.
+ */
+                if (sc.sc_eval == PAM_AUTH_ERR) {                
+/*
+ * Reenter loop, if PAM_AUTH_ERR condition halts. 
+ */
+                    cnt += 1;    
+        
+                    if (cnt > backoff) 
+                        (void)sleep((u_int)((cnt - backoff) * 5));
+        
+                    if (cnt >= retries)
+                        ask = 0;        
+    
+                    (void)pam_end(sc.sc_pamh, sc.sc_eval);
+        
+                    sc.sc_pamh = NULL;
+                } else
+                    ask = 0;    
+            } else
+                ask = 0;    
+        }    
+    }
+/*
+ * Close pam(8) session, if any.
+ */
     if (sc.sc_pamh != NULL)
-        (void)pam_end(sc.sc_pamh, sc.sc_eval);
-
+        (void)pam_end(sc.sc_pamh, sc.sc_eval);  
+out:
+/*
+ * Create response.
+ */            
+    if (sc.sc_eval == PAM_SUCCESS) 
+        resp = SOD_AUTH_ACK;
+    else 
+        resp = SOD_AUTH_REJ; 
+            
+    sod_msg_prepare(sc.sc_user, resp, &sc.sc_buf);
+    
+    (void)sod_msg_fn(sod_msg_send, sc.sc_sock_rmt, &sc.sc_buf);
+    
     (void)memset(&sc, 0, sizeof(sc));
 
 #ifdef SOD_DEBUG        
@@ -266,37 +364,7 @@ sod_establish(struct sod_softc *sc)
 syslog(LOG_DEBUG, "%s\n", __func__);
 #endif /* SOD_DEBUG */    
     
-    if (sod_msg_fn(sod_msg_recv, sc->sc_sock_rmt, &sc->sc_buf) < 0) 
-        goto out;
-/*
- * Create < hostname, user > tuple.
- */
-    if (gethostname(sc->sc_host, SOD_NMAX) < 0) 
-        goto out;    
- 
-    (void)strncpy(sc->sc_user, sc->sc_buf.sm_tok, SOD_NMAX);
-/*
- * Verify, if username exists in passwd database. 
- */
-    if ((sc->sc_pwd = getpwnam(sc->sc_user)) != NULL) {
-/*
- * Verify, if user has UID 0, because login by UID 0 is not allowed. 
- */
-        if (sc->sc_pwd->pw_uid == (uid_t)0) 
-            sc->sc_eval = PAM_PERM_DENIED;
-        else
-            sc->sc_eval = PAM_SUCCESS;
-    } else 
-        sc->sc_eval = PAM_USER_UNKNOWN;
-    
-    endpwent();          
-/*
- * State transition, if any.
- */
-    if (sc->sc_eval == PAM_SUCCESS) {
-        if (sc->sc_buf.sm_code == SOD_AUTH_REQ) 
-            state = (sod_state_fn_t)sod_authenticate;
-    }
+
 out:    
     return (state);
 }
@@ -307,87 +375,14 @@ out:
 static sod_state_fn_t  
 sod_authenticate(struct sod_softc *sc)
 {      
-    login_cap_t *sc_lc;
     
-    const char     *prompt;
-    const char     *pw_prompt;
-    
-    int retries, backoff;
-    int ask = 1, cnt = 0;
-    uint32_t resp;
 
 #ifdef SOD_DEBUG        
 syslog(LOG_DEBUG, "%s\n", __func__);
 #endif /* SOD_DEBUG */
 
-/*
- * Parts of in login.c defined codesections are reused here.
- */    
-    lc = login_getclass(NULL);
-    prompt = login_getcapstr(lc, "login_prompt", 
-        sod_prompt_default, sod_prompt_default);
-    pw_prompt = login_getcapstr(lc, "passwd_prompt", 
-        sod_pw_prompt_default, sod_pw_prompt_default);
-    retries = login_getcapnum(lc, "login-retries", 
-        SOD_RETRIES_DFLT, SOD_RETRIES_DFLT);
-    backoff = login_getcapnum(lc, "login-backoff", 
-        SOD_BACKOFF_DFLT, SOD_BACKOFF_DFLT);
-    login_close(lc);
-    lc = NULL;
-       
-    while (ask != 0) {
-/*
- * Open pam(8) session and authenticate.
- */        
-        sc->sc_eval = pam_start(sod_name, sc->sc_user, 
-            &sc->sc_pamc, &sc->sc_pamh);
 
-        if (sc->sc_eval == PAM_SUCCESS) {
-            sc->sc_eval = pam_set_item(sc->sc_pamh, PAM_RUSER, 
-                sc->sc_user);
-        }
-    
-        if (sc->sc_eval == PAM_SUCCESS) {
-            sc->sc_eval = pam_set_item(sc->sc_pamh, PAM_RHOST, 
-                sc->sc_host);
-        }
-    
-        if (sc->sc_eval == PAM_SUCCESS) {
-            sc->sc_eval = pam_authenticate(sc->sc_pamh, 0);
-/*
- * Authenticate.
- */
-            if (sc->sc_eval == PAM_AUTH_ERR) {                
-/*
- * Reenter loop, if PAM_AUTH_ERR condition halts. 
- */
-                cnt += 1;    
-        
-                if (cnt > backoff) 
-                    (void)sleep((u_int)((cnt - backoff) * 5));
-        
-                if (cnt >= retries)
-                    ask = 0;        
-    
-                (void)pam_end(sc->sc_pamh, sc->sc_eval);
-        
-                sc->sc_pamh = NULL;
-            } else
-                ask = 0;    
-        } else
-            ask = 0;    
-    }
-/*
- * Create response.
- */            
-    if (sc->sc_eval == PAM_SUCCESS) 
-        resp = SOD_AUTH_ACK;
-    else 
-        resp = SOD_AUTH_REJ; 
-            
-    sod_msg_prepare(sc->sc_user, resp, &sc->sc_buf);
-    
-    (void)sod_msg_fn(sod_msg_send, sc->sc_sock_rmt, &sc->sc_buf);
+
 out:    
     return (NULL);
 }
